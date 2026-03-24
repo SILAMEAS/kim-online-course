ceb3ddc9d720b50fc076f1790afa12dcc078306b
Sila MEAS <sila.meas@allweb.com.kh>
2026-03-24 11:38:00 +0700

    add-dot-end-for-openapi-config.cjs

--- openapi-config.cjs
+++ openapi-config.cjs
@@ -1,5 +1,6 @@
+require("dotenv").config();
 module.exports = {
-  schemaFile: "http://localhost:3005/v3/api-docs",
+  schemaFile: process.env.VITE_API_URL + "/v3/api-docs",
   apiFile: "./src/lib/api/apiSlice.ts",
   apiImport: "apiSlice",
   outputFile: "./src/lib/api/api.generated.ts",

c3619e836645096bb995d89c2558d357c900c76b
Sila MEAS <sila.meas@allweb.com.kh>
2026-04-23 12:09:31 +0700

    translate duration time

--- dist/assets/formatDurationVideo-RTkKD0z4.js
+++
@@ -1,1 +0,0 @@
-const a=r=>{if(!r)return"0m";const o=Math.floor(r/3600),t=Math.floor(r%3600/60),f=Math.floor(r%60);return o>0?`${o}h ${t}m`:t>0?`${t}m ${f}s`:`${f}s`};export{a as f};
--- src/lib/utils/formatDurationVideo.ts
+++ src/lib/utils/formatDurationVideo.ts
@@ -1,11 +1,18 @@
+import i18n from "@/i18n";
+import {Localization} from "@/i18n/lang";
+
 export const formatDurationVideo = (seconds?: number) => {
-    if (!seconds) return "0m";
+    const _m= i18n.t(Localization("duration_format","m"));
+    const _h= i18n.t(Localization("duration_format","h"));
+    const _s= i18n.t(Localization("duration_format","s"));
+
+    if (!seconds) return `0 ${_s}`;
 
     const h = Math.floor(seconds / 3600);
     const m = Math.floor((seconds % 3600) / 60);
     const s = Math.floor(seconds % 60);
 
-    if (h > 0) return `${h}h ${m}m`;
-    if (m > 0) return `${m}m ${s}s`;
-    return `${s}s`;
+    if (h > 0) return `${h} ${_h} ${m} ${_m}`;
+    if (m > 0) return `${m} ${_m} ${s} ${_s}`;
+    return `${s} ${_s}`;
 };
